1245d8ee17f1c44853baf4cf937d12647933fbeb
Henry <hrmstockdale@gmail.com>
2019-06-11 15:41:48 +0100

    styled and positioned modal

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,38 @@
-import React from 'react';
-import './App.css';
-import { Clouds, Landing, Modal, OpenModal } from './Components';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import GlobalStyle from './GlobalStyle';
+import React from "react";
+import "./App.css";
+import { Clouds, Landing, Modal, OpenModal } from "./Components";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import GlobalStyle from "./GlobalStyle";
 
 function App() {
-
-	return (
-		<BrowserRouter>
-			<GlobalStyle />
-			<div className="App">
-				<Clouds />
-				<Landing />
-				<OpenModal
-					toggle={(show) => <button onClick={show}>Learn about LSx</button>}
-					content={(hide) => (
-						<Modal>
-							<h2>About LSx</h2>
-							This is where the modal text is.
-							<br />
-							<button onClick={hide}>X</button>
-						</Modal>
-					)}
-				/>
-			</div>
-		</BrowserRouter>
-	);
-
+  return (
+    <BrowserRouter>
+      <GlobalStyle />
+      <div className='App'>
+        <Clouds />
+        <Landing />
+        <OpenModal
+          toggle={show => <button onClick={show}>Learn about LSx</button>}
+          content={hide => (
+            <Modal>
+              <button onClick={hide}>X</button>
+              <h2>
+                About <a href='http://www.lsx.org.uk/'>LSx</a>
+              </h2>
+              <p>
+                London Sustainability Exchange is a “think and do” charity
+                <br />
+                which creates collaborations that address the complex barriers
+                to
+                <br />a sustainable London.
+              </p>
+              <br />
+            </Modal>
+          )}
+        />
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- client/src/Components/Modal/Modal.style.js
+++ client/src/Components/Modal/Modal.style.js
@@ -1,13 +1,18 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Modal = styled.section`
-    position: fixed;
-    top: 20%;
-    left: 30%;
-    background-color: white;
-    border: 3px solid pink;
-`
+  padding: 30px;
+  position: fixed;
+  left: 20%;
+  right: 20%;
+  bottom: 25%;
+  background-color: white;
+  margin: 15px;
+  transform: rotate(-2deg);
+  border: 1px solid;
+  border-width: 5px 3px 3px 5px;
+  border-radius: 95% 4% 97% 5%/4% 94% 3% 95%;
+  opacity: 0.8;
+`;
 
-export {
-    Modal
-};
+export { Modal };
