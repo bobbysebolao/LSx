c221dda323edcbcff68e853dbb1de2f33b5d7fde
Henry <hrmstockdale@gmail.com>
2019-06-11 10:29:01 +0100

    see aabove commit

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,18 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Clouds, Landing } from "./Components";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import GlobalStyle from "./GlobalStyle";
 
 function App() {
+  const backgroundUrls = {
+    home: "../public/content/city-day-edited.svg",
+    cat:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Neighbours_Siamese.jpg/640px-Neighbours_Siamese.jpg"
+  };
+
   return (
     <BrowserRouter>
+      <GlobalStyle {...backgroundUrls} />
       <div className='App'>
         <Clouds />
         <Landing />
--- client/src/Components/Landing/Landing.js
+++ client/src/Components/Landing/Landing.js
@@ -1,14 +1,13 @@
 import React from "react";
 import Button from "../Buttons/Button.js";
-import { Background } from "./Landing.style.js";
 
 const Landing = props => {
   return (
-    <Background>
+    <section>
       <h1>LSx</h1>
       <h2>Action Planner</h2>
       <Button />
-    </Background>
+    </section>
   );
 };
 
