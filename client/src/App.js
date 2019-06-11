11abc0f8638289b4fd1f9dc81278aaffcc33aa4e
Joko Sanyang <jo.san@live.co.uk>
2019-06-11 12:07:49 +0100

    Create modal component with style

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,31 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { Clouds, Landing } from "./Components";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import GlobalStyle from "./GlobalStyle";
+import React from 'react';
+import './App.css';
+import { Clouds, Landing, Modal, OpenModal } from './Components';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import GlobalStyle from './GlobalStyle';
 
 function App() {
-  const backgroundUrls = {
-    home: "../public/content/city-day-edited.svg",
-    cat:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Neighbours_Siamese.jpg/640px-Neighbours_Siamese.jpg"
-  };
-
-  return (
-    <BrowserRouter>
-      <GlobalStyle {...backgroundUrls} />
-      <div className='App'>
-        <Clouds />
-        <Landing />
-        <p>Hello World!</p>
-      </div>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<GlobalStyle />
+			<div className="App">
+				<Clouds />
+				<Landing />
+				<OpenModal
+					toggle={(show) => <button onClick={show}>Learn about LSx</button>}
+					content={(hide) => (
+						<Modal>
+							<h2>About LSx</h2>
+							This is where the modal text is.
+							<br />
+							<button onClick={hide}>X</button>
+						</Modal>
+					)}
+				/>
+				<p>Hello World!</p>
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
