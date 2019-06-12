81fd2e22d8630a5c0a1b4887f511c4a26907c4ea
Bobby <bobbysebolao@gmail.com>
2019-06-12 15:56:20 +0100

    Link 'Learn About LSx' modal to sign-five in the SVG

--- client/src/Components/Landing/Landing.js
+++ client/src/Components/Landing/Landing.js
@@ -1,48 +1,46 @@
-import React from 'react';
-import Button from '../Buttons/Button.js';
+import React from "react";
+import Button from "../Buttons/Button.js";
 import { Clouds } from "../../Components";
-import { Modal, OpenModal } from '../Modal/Modal';
+import { Modal, OpenModal } from "../Modal/Modal";
 // import { ReactComponent as Sign } from '../../content/signpost-edited.svg';
 import Sign from "./Signpost/Sign";
-import * as S from './Landing.style';
+import * as S from "./Landing.style";
 
-const Landing = (props) => {
-	return (
-		<S.Main>
+const Landing = props => {
+  return (
+    <S.Main>
       <Clouds />
-			<h1>LSx</h1>
-			<h2>Action Planner</h2>
-			<Button> Start Learning </Button>
-			<Button> Make Action Plan </Button>
-			<S.Nav>
-				<OpenModal
-          toggle={(show) => (
-          <React.Fragment>
-          <Sign onClick={show}></Sign> 
-          </React.Fragment>
+      <h1>LSx</h1>
+      <h2>Action Planner</h2>
+      <Button> Start Learning </Button>
+      <Button> Make Action Plan </Button>
+      <S.Nav>
+        <OpenModal
+          toggle={show => (
+            <React.Fragment>
+              <Sign visibility={show} />
+            </React.Fragment>
           )}
-					content={(hide) => (
-						<Modal>
-							<button onClick={hide}>X</button>
-							<h2>
-								About <a href="http://www.lsx.org.uk/">LSx</a>
-							</h2>
-							<p>
-								London Sustainability Exchange is a “think and do” charity
-								<br />
-								which creates collaborations that address the complex barriers
-								to
-								<br />a sustainable London.
-							</p>
-							<br />
-						</Modal>
-					)}
-				/>
-
-				
-			</S.Nav>
-		</S.Main>
-	);
+          content={hide => (
+            <Modal>
+              <button onClick={hide}>X</button>
+              <h2>
+                About <a href="http://www.lsx.org.uk/">LSx</a>
+              </h2>
+              <p>
+                London Sustainability Exchange is a “think and do” charity
+                <br />
+                which creates collaborations that address the complex barriers
+                to
+                <br />a sustainable London.
+              </p>
+              <br />
+            </Modal>
+          )}
+        />
+      </S.Nav>
+    </S.Main>
+  );
 };
 
 export default Landing;
