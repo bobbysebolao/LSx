40bfd224e9a8b11b4014c3caa9cbb1f66eca989a
Joko Sanyang <jo.san@live.co.uk>
2019-06-12 12:58:10 +0100

    Move modal into landing component, add text to signpost and links to page

--- client/src/Components/Landing/Landing.js
+++ client/src/Components/Landing/Landing.js
@@ -1,21 +1,47 @@
-import React from "react";
-import Button from "../Buttons/Button.js";
-import {ReactComponent as Sign}  from "../../content/signpost-edited.svg";
-import * as S from "./Landing.style";
+import React from 'react';
+import Button from '../Buttons/Button.js';
+import { Clouds } from "../../Components";
+import { Modal, OpenModal } from '../Modal/Modal';
+import { ReactComponent as Sign } from '../../content/signpost-edited.svg';
+import * as S from './Landing.style';
 
+const Landing = (props) => {
+	return (
+		<S.Main>
+      <Clouds />
+			<h1>LSx</h1>
+			<h2>Action Planner</h2>
+			<Button> Start Learning </Button>
+			<Button> Make Action Plan </Button>
+			<S.Nav>
+				<OpenModal
+          toggle={(show) => (
+          <React.Fragment>
+          <Sign onClick={show}></Sign> 
+          </React.Fragment>
+          )}
+					content={(hide) => (
+						<Modal>
+							<button onClick={hide}>X</button>
+							<h2>
+								About <a href="http://www.lsx.org.uk/">LSx</a>
+							</h2>
+							<p>
+								London Sustainability Exchange is a “think and do” charity
+								<br />
+								which creates collaborations that address the complex barriers
+								to
+								<br />a sustainable London.
+							</p>
+							<br />
+						</Modal>
+					)}
+				/>
 
-const Landing = props => {
-  return (
-    <S.Main>
-      <h1>LSx</h1>
-      <h2>Action Planner</h2>
-      <Button> Start Learning </Button>
-      <Button> Make Action Plan </Button>
-      <S.Nav>
-      <Sign />
-      </S.Nav>
-    </S.Main>
-  );
+				
+			</S.Nav>
+		</S.Main>
+	);
 };
 
 export default Landing;
