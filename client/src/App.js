8ab9d3eba2f7e9702fb99dc489ed83d8c70931d4
Henry <hrmstockdale@gmail.com>
2019-06-13 16:17:25 +0100

    404 page component made, routed in app at bottom of switch case and created Oldlady comp for use elsewhere + in 404 itself

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,48 @@
-
-import React from 'react';
-import './App.css';
-import { Landing, Stories, Form, Deepdive } from './Components';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import GlobalStyle from './GlobalStyle';
+import React from "react";
+import "./App.css";
+import { Landing, Stories, Form, Deepdive, Error404 } from "./Components";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import GlobalStyle from "./GlobalStyle";
 
 const urls = {
-	home: '/',
-	stories: '/success-stories',
-	deepdive: '/dive-deeper',
-	experiments: '/experiments',
-	action: '/action-plan',
-	share: '/share'
+  home: "/",
+  stories: "/success-stories",
+  deepdive: "/dive-deeper",
+  experiments: "/experiments",
+  action: "/action-plan",
+  share: "/share"
 };
 
 function App() {
-	const [background, setBackground] = React.useState('city');
-	return (
-		<BrowserRouter>
-			<GlobalStyle data={background} />
-			<div className="App">
-				<Switch>
-					<Route exact path={urls.home} render={(props) => <Landing/>} />
-					<Route exact path={urls.action} render={(props) => <Form/>} />
-					<Route
-						exact
-						path={urls.stories}
-						render={(props) => {
-							setBackground('comic');
-							return <Stories />;
-						}}
-					/>
-          <Route exact path={urls.deepdive} render={(props) => {
-          setBackground('park')
-          return (<Deepdive />);}} />
-				</Switch>
-			</div>
-		</BrowserRouter>
-	);
+  const [background, setBackground] = React.useState("city");
+  return (
+    <BrowserRouter>
+      <GlobalStyle data={background} />
+      <div className='App'>
+        <Switch>
+          <Route exact path={urls.home} render={props => <Landing />} />
+          <Route exact path={urls.action} render={props => <Form />} />
+          <Route
+            exact
+            path={urls.stories}
+            render={props => {
+              setBackground("comic");
+              return <Stories />;
+            }}
+          />
+          <Route
+            exact
+            path={urls.deepdive}
+            render={props => {
+              setBackground("park");
+              return <Deepdive />;
+            }}
+          />
+          <Route component={Error404} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- client/src/Components/index.js
+++ client/src/Components/index.js
@@ -1,6 +1,7 @@
 import Landing from "./Landing/Landing.js";
 import Stories from "./Stories/Stories.js";
 import { Form } from "./Form/Form.js";
-import Deepdive from "./Deepdive/Deepdive.js"
+import Deepdive from "./Deepdive/Deepdive.js";
+import Error404 from "./Error404/Error404";
 
-export { Landing, Stories, Form, Deepdive };
+export { Landing, Stories, Form, Deepdive, Error404 };
