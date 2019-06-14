42916374daa5f8fe844431e4e729121c00f9c76d
dubhcait <dubhcait@live.ie>
2019-06-14 11:19:18 +0100

    add get route in express server

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const bodyParser = require("body-parser");
 
 const sendActionPlan = require("./sendActionPlan");
+const fetchSuccessData = require("./getSuccessStories");
 
 require("env2")("./.env");
 
@@ -16,6 +17,13 @@ app.use(express.static(path.join(__dirname, "..", "client", "build")));
 
 app.get("/express_backend", (req, res) => {
   res.send({ express: " YOUR BACKEND IS CONNECTED TO REACT" });
+});
+
+app.get("/success-data", (req, res) => {
+
+  fetchSuccessData() 
+  .then(response => {
+    res.send(response)})
 });
 
 app.get("*", (req, res) => {
