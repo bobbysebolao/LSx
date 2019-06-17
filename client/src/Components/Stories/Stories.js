dd78b0cb60e0e7da56eb0dc4527671f3d4e6269d
Bobby <bobbysebolao@gmail.com>
2019-06-17 14:46:36 +0100

    Remove extra comic from Stories.js render

--- client/src/Components/Stories/Stories.js
+++ client/src/Components/Stories/Stories.js
@@ -1,33 +1,35 @@
-import React from 'react';
-import * as S from './Stories.style';
+import React from "react";
+import * as S from "./Stories.style";
 
 const Stories = ({ data }) => {
-const [storiesData, setStoriesData ] = React.useState(null);
+  // const [storiesData, setStoriesData] = React.useState(null);
 
-	React.useEffect(() => {
-		if (data) {
-			console.log('Hello', data);	
-			setStoriesData(Object.keys(data));	
-			console.log("this is storiesdata", storiesData);		
-	}
-	}, [data]);
+  React.useEffect(
+    () => {
+      if (data) {
+        console.log("Hello", data);
+        // setStoriesData(Object.keys(data));
+        // console.log("this is storiesdata", storiesData);
+      }
+    },
+    [data]
+  );
 
-	return (
-		<S.Main>
-			<S.Header>
-				<h2>Success Stories</h2>
-				<p>
-					Click on the comic books below to read about the everyday heroes doing
-					their bit to promote clean air in their communities.
-				</p>
-			</S.Header>
-			{/* Foreach row in the spreadsheet, generate new book, using the incrementer to make an id and give the x and y co-ordinate somehow*/}
-			<S.Shelf>
-				{ storiesData ? storiesData.map(k=> <S.Comic key={k}></S.Comic>) : null }
-        <S.Comic /> */}
-			</S.Shelf>
-		</S.Main>
-	);
+  return (
+    <S.Main>
+      <S.Header>
+        <h2>Success Stories</h2>
+        <p>
+          Click on the comic books below to read about the everyday heroes doing
+          their bit to promote clean air in their communities.
+        </p>
+      </S.Header>
+      {/* Foreach row in the spreadsheet, generate new book, using the incrementer to make an id and give the x and y co-ordinate somehow*/}
+      <S.Shelf>
+        {data ? data.map((k, i) => <S.Comic key={k["Title"]} />) : null}
+      </S.Shelf>
+    </S.Main>
+  );
 };
 
 export default Stories;
