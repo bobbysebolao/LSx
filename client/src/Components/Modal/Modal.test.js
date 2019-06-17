676e41638fbcbcd5cfc0e8f695fe2392b9920667
Henry <hrmstockdale@gmail.com>
2019-06-17 11:50:01 +0100

    form fields added to Form and sendActionPlan updated to handle answers object, q1 - q5

--- client/src/Components/Modal/Modal.test.js
+++ client/src/Components/Modal/Modal.test.js
@@ -1,36 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { render , fireEvent, cleanup } from '@testing-library/react';
-import { Modal, OpenModal } from './Modal';
-import * as S from "./Modal.style";
+import React from "react";
+
+import { render, fireEvent, cleanup } from "@testing-library/react";
+import { Modal, OpenModal } from "./Modal";
 
 afterEach(cleanup);
 
+test(" the modal opens when clicked", () => {
+  const modalRoot = document.createElement("div");
+  modalRoot.setAttribute("id", "modal-root");
+  document.body.appendChild(modalRoot);
 
+  const { getByText } = render(
+    <OpenModal
+      toggle={show => <button onClick={show}>Learn about LSx</button>}
+      content={hide => (
+        <Modal>
+          <button onClick={hide}>X</button>
+          <h2>
+            Testing <a href='http://www.lsx.org.uk/'>LSx</a>
+          </h2>
+        </Modal>
+      )}
+    />
+  );
 
-test(' the modal opens when clicked', () => {
-   
-    const modalRoot = document.createElement('div');
-    modalRoot.setAttribute('id', 'modal-root');
-    document.body.appendChild(modalRoot);
-
-
-    const { getByText } = render( <OpenModal toggle={show => (
-          <button onClick={show}>Learn about LSx</button>
-        )}
-        content={hide => (
-  <Modal>
-    <button onClick={hide}>X</button>
-    <h2>
-      Testing <a href='http://www.lsx.org.uk/'>LSx</a>
-    </h2>
-    
-  </Modal>
-        )}
-      />);
-
-      const buttonNode = getByText(/Learn about LSx/i);
-      fireEvent.click(buttonNode);
-      getByText('Testing'); 
-
+  const buttonNode = getByText(/Learn about LSx/i);
+  fireEvent.click(buttonNode);
+  getByText("Testing");
 });
