ea73b3f819336161a3feebc35b23ed64d56d09ea
Joko Sanyang <jo.san@live.co.uk>
2019-06-17 13:17:27 +0100

    Render comics based on object items

--- client/src/Components/Stories/Stories.js
+++ client/src/Components/Stories/Stories.js
@@ -1,32 +1,38 @@
-import React from "react";
-import * as S from "./Stories.style";
+import React from 'react';
+import * as S from './Stories.style';
 
 const Stories = ({ data }) => {
-  React.useEffect(
-    () => {
-      if (data) {
-        console.log("Hello", Object.keys(data).length);
-      }
-    },
-    [data]
-  );
+const [storiesData, setStoriesData ] = React.useState(null);
+	// const renderData = data => {
+	// 	data.map(story => {
+	// 		return <S.Comic />
+	// })
+	// }
+	React.useEffect(() => {
+		if (data) {
+			console.log('Hello', data);	
+			setStoriesData(Object.keys(data));	
+			console.log("this is storiesdata", storiesData);		
+	}
+	}, [data]);
 
-  return (
-    <S.Main>
-      <S.Header>
-        <h2>Success Stories</h2>
-        <p>
-          Click on the comic books below to read about the everyday heroes doing
-          their bit to promote clean air in their communities.
-        </p>
-      </S.Header>
-      {/* Foreach row in the spreadsheet, generate new book, using the incrementer to make an id and give the x and y co-ordinate somehow*/}
-      <S.Shelf>
-        <S.ComicOne />
-        <S.ComicTwo />
-      </S.Shelf>
-    </S.Main>
-  );
+	return (
+		<S.Main>
+			<S.Header>
+				<h2>Success Stories</h2>
+				<p>
+					Click on the comic books below to read about the everyday heroes doing
+					their bit to promote clean air in their communities.
+				</p>
+			</S.Header>
+			{/* Foreach row in the spreadsheet, generate new book, using the incrementer to make an id and give the x and y co-ordinate somehow*/}
+			<S.Shelf>
+				{ storiesData ? storiesData.map(k=> <S.Comic key={k}></S.Comic>) : console.log("nope")}
+				{/* <S.Comic style={{backgroundColor:'blue'}} />
+        <S.Comic /> */}
+			</S.Shelf>
+		</S.Main>
+	);
 };
 
 export default Stories;
