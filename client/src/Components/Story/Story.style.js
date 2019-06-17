d1da8386ef32ab53aee364d80ededc3d28f1016d
Bobby <bobbysebolao@gmail.com>
2019-06-17 10:46:59 +0100

    Style story.style.js background pattern

--- client/src/Components/Story/Story.style.js
+++ client/src/Components/Story/Story.style.js
@@ -7,12 +7,15 @@ const Wrapper = styled.div`
 	width: 320px;
 	border: 1px solid red;
 	padding: 12px 12px 0 12px;
+	background-color: #ffe4c3;
+background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='4' height='4' viewBox='0 0 4 4'%3E%3Cpath fill='%23397d9f' fill-opacity='0.09' d='M1 3h1v1H1V3zm2-2h1v1H3V1z'%3E%3C/path%3E%3C/svg%3E");
 `;
 
 const Header = styled.header`
 	display: grid;
 	grid-template-rows: 1fr 0.5fr 0.5fr;
 	border: 3px solid black;
+	background: white;
 `;
 
 const TitleTop = styled.h1`
