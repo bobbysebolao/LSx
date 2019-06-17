f41f8e35d8e1ff79a1748be5e81febc7b81f5983
Bobby <bobbysebolao@gmail.com>
2019-06-17 11:27:33 +0100

    Refactor airtable query to get all success stories data

--- client/src/Components/Stories/Stories.js
+++ client/src/Components/Stories/Stories.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import * as S from './Stories.style';
+import React from "react";
+import * as S from "./Stories.style";
 
 const Stories = ({ data }) => {
+  console.log("Hello", data);
 
-console.log(data)
-
-	return (
-		<S.Main>
-			<S.Header>
-				<h2>Success Stories</h2>
-				<p>
-					Click on the comic books below to read about the everyday heroes doing
-					their bit to promote clean air in their communities.
-				</p>
-			</S.Header>
-    {/* Foreach row in the spreadsheet, generate new book, using the incrementer to make an id and give the x and y co-ordinate somehow*/}
+  return (
+    <S.Main>
+      <S.Header>
+        <h2>Success Stories</h2>
+        <p>
+          Click on the comic books below to read about the everyday heroes doing
+          their bit to promote clean air in their communities.
+        </p>
+      </S.Header>
+      {/* Foreach row in the spreadsheet, generate new book, using the incrementer to make an id and give the x and y co-ordinate somehow*/}
       <S.Shelf>
-			<S.ComicOne />
-			<S.ComicTwo	/>
+        <S.ComicOne />
+        <S.ComicTwo />
       </S.Shelf>
-		</S.Main>
-	);
+    </S.Main>
+  );
 };
 
 export default Stories;
