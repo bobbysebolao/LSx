6de1df7c068d9d2fae3969abbcc47d27a99a1ba7
Henry <hrmstockdale@gmail.com>
2019-06-19 17:26:47 +0100

    border radius added to landing and experiements pages

--- client/src/Components/Landing/Landing.style.js
+++ client/src/Components/Landing/Landing.style.js
@@ -1,25 +1,26 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Nav = styled.section`
-	margin: auto;
-	margin-top: 950px;
-	width: 320px;
+  margin: auto;
+  margin-top: 950px;
+  width: 320px;
 `;
 
 const Main = styled.main`
-	width: 100%;
+  width: 100%;
 `;
 
 const ButtonContainer = styled.div`
-	display: flex;
-	flex-direction: column;
+  display: flex;
+  flex-direction: column;
 `;
 
 const NavDescription = styled.div`
-	padding: 0.75em;
-	margin: 0.75em;
-	background-color: #fef2e3;
-	opacity: 0.8;
+  padding: 0.75em;
+  margin: 0.75em;
+  background-color: #fef2e3;
+  opacity: 0.8;
+  border-radius: 5px;
 `;
 
 const Link = styled.a`
@@ -28,19 +29,19 @@ const Link = styled.a`
 `;
 
 const Button = styled.button`
-	margin-left: 200px;
-	border: none;
-	background-color: #cb4e4e;
-	font-family: inherit;
-	font-size: inherit;
-	color: inherit;
-	cursor: pointer;
-	padding: 0.5rem 0.7rem;
-	text-transform: uppercase;
-	letter-spacing: 1px;
-	font-weight: 700;
-	outline: none;
-    border-radius: 5px;
+  margin-left: 200px;
+  border: none;
+  background-color: #cb4e4e;
+  font-family: inherit;
+  font-size: inherit;
+  color: inherit;
+  cursor: pointer;
+  padding: 0.5rem 0.7rem;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-weight: 700;
+  outline: none;
+  border-radius: 5px;
 `;
 
 export { Nav, Main, ButtonContainer, NavDescription, Link, Button };
