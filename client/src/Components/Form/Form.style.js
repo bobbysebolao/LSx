3229f0235315a53fb884b3dd09446c2109fb977b
dubhcait <dubhcait@live.ie>
2019-06-19 15:31:13 +0100

    new background

--- client/src/Components/Form/Form.style.js
+++ client/src/Components/Form/Form.style.js
@@ -1,13 +1,14 @@
 import styled from "styled-components";
 
+
 const Wrapper = styled.div`
   position: relative;
   margin: auto;
   text-align: center;
   width: 320px;
-  border: 1px solid red;
+  border: 1px solid black;
   padding: 12px 12px 0 12px;
-  background-color: #ffe4c3;
+  background-color: #ebe7db;
   background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='4' height='4' viewBox='0 0 4 4'%3E%3Cpath fill='%23397d9f' fill-opacity='0.09' d='M1 3h1v1H1V3zm2-2h1v1H3V1z'%3E%3C/path%3E%3C/svg%3E");
 `;
 
