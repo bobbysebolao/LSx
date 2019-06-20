12274e1ff6e942ab0b653360af853d2b0022169f
Henry <hrmstockdale@gmail.com>
2019-06-20 15:44:30 +0100

    styled Comic comps in Story page comp, chose blue from submit buttons for uniformity

--- client/src/Components/Story/Story.js
+++ client/src/Components/Story/Story.js
@@ -1,60 +1,62 @@
-import React from 'react';
-import * as S from './Story.style';
-import hyphenate from '../../utils/hyphenate.js';
-import { Button } from '../Buttons/Button.js';
+import React from "react";
+import * as S from "./Story.style";
+import hyphenate from "../../utils/hyphenate.js";
+import { Button } from "../Buttons/Button.js";
 
-const Story = (props) => {
-	const [storyData, setStoryData] = React.useState(null);
+const Story = props => {
+  const [storyData, setStoryData] = React.useState(null);
 
-	const selectStory = (airtableStories) => {
-		if (airtableStories.data !== null) {
-			const correctStory = airtableStories.data.filter(
-				(story) => {
-					return hyphenate(story['title']).toLowerCase() === airtableStories.match.params.story
-        });
-			setStoryData(correctStory[0]);
-		}
-	};
+  const selectStory = airtableStories => {
+    if (airtableStories.data !== null) {
+      const correctStory = airtableStories.data.filter(story => {
+        return (
+          hyphenate(story["title"]).toLowerCase() ===
+          airtableStories.match.params.story
+        );
+      });
+      setStoryData(correctStory[0]);
+    }
+  };
 
-	React.useEffect(() => {
-		selectStory(props);
-	}, [props]);
+  React.useEffect(() => {
+    selectStory(props);
+  }, [props]);
 
-	if (storyData) {
-		console.log("storyData = ", storyData);
-	}
+  if (storyData) {
+    console.log("storyData = ", storyData);
+  }
 
-	return (
-		<S.Wrapper>
-			<S.Header>
-				<S.TitleTop>SUSTAINABILITY</S.TitleTop>
-				<S.TitleBottom>HEROES</S.TitleBottom>
-				<S.Issue>ISSUE #1</S.Issue>
-				<S.Date>4 JUNE</S.Date>
-				<S.Subtitle>Citizen science success stories</S.Subtitle>
-			</S.Header>
+  return (
+    <S.Wrapper>
+      <S.Header>
+        <S.TitleTop>SUSTAINABILITY</S.TitleTop>
+        <S.TitleBottom>HEROES</S.TitleBottom>
+        <S.Issue>ISSUE #1</S.Issue>
+        <S.Date>4 JUNE</S.Date>
+        <S.Subtitle>Citizen science success stories</S.Subtitle>
+      </S.Header>
 
-			<h2>{storyData ? storyData['title'] : `Loading`}</h2>
-			<section>
-				<ul>
-					<li>
-						{storyData
-							? storyData['Key Points (each one on new line)']
-							: `Loading`}
-					</li>
-					<li>Lorem ipsum dolor sit amet</li>
-					<li>Lorem ipsum dolor sit amet</li>
-				</ul>
-			</section>
-			<img
-				width="100%"
-				src={storyData ? `${storyData['image'][0]['url']}` : ``}
-				alt="description"
-			/>
-			<section>{storyData ? storyData['Description'] : `Loading`}</section>
-			<Button link="/success-stories">Back</Button>
-		</S.Wrapper>
-	);
+      <h2>{storyData ? storyData["title"] : `Loading`}</h2>
+      <section>
+        <ul>
+          <li>
+            {storyData
+              ? storyData["Key Points (each one on new line)"]
+              : `Loading`}
+          </li>
+          <li>Lorem ipsum dolor sit amet</li>
+          <li>Lorem ipsum dolor sit amet</li>
+        </ul>
+      </section>
+      <img
+        width='100%'
+        src={storyData ? `${storyData["image"][0]["url"]}` : ``}
+        alt='description'
+      />
+      <section>{storyData ? storyData["Description"] : `Loading`}</section>
+      <Button link='/success-stories'>Back</Button>
+    </S.Wrapper>
+  );
 };
 
 export default Story;
--- client/src/Components/Story/Story.style.js
+++ client/src/Components/Story/Story.style.js
@@ -1,69 +1,69 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Wrapper = styled.div`
-	position: relative;
-	margin: auto;
-	text-align: center;
-	width: 320px;
-	border: 1px solid red;
-	padding: 12px 12px 0 12px;
-	background-color: #ffe4c3;
-background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='4' height='4' viewBox='0 0 4 4'%3E%3Cpath fill='%23397d9f' fill-opacity='0.09' d='M1 3h1v1H1V3zm2-2h1v1H3V1z'%3E%3C/path%3E%3C/svg%3E");
+  position: relative;
+  margin: auto;
+  text-align: center;
+  width: 320px;
+  border: 1px solid red;
+  padding: 12px 12px 0 12px;
+  background-color: #ffe4c3;
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='4' height='4' viewBox='0 0 4 4'%3E%3Cpath fill='%23397d9f' fill-opacity='0.09' d='M1 3h1v1H1V3zm2-2h1v1H3V1z'%3E%3C/path%3E%3C/svg%3E");
 `;
 
 const Header = styled.header`
-	display: grid;
-	grid-template-rows: 1fr 0.5fr 0.5fr;
-	border: 3px solid black;
-	background: white;
+  display: grid;
+  grid-template-rows: 1fr 0.5fr 0.5fr;
+  border: 3px solid black;
+  background: white;
 `;
 
 const TitleTop = styled.h1`
-	grid-column: 1 / 5;
-	grid-row: 1 / 2;
-	font-size: 2.25em;
-	margin-bottom: 0;
+  grid-column: 1 / 5;
+  grid-row: 1 / 2;
+  font-size: 2.25em;
+  margin-bottom: 0;
 `;
 
 const TitleBottom = styled.h1`
-	grid-column: 2 / 4;
-	grid-row: 2 / 3;
-	font-size: 2.25em;
-	margin-top: 0;
-	margin-bottom: 0;
+  grid-column: 2 / 4;
+  grid-row: 2 / 3;
+  font-size: 2.25em;
+  margin-top: 0;
+  margin-bottom: 0;
 `;
 
 const Issue = styled.p`
-	grid-column: 1 / 2;
-	grid-row: 2 / 3;
-	margin-top: 0;
-	margin-bottom: 0;
-    align-self: center;
-    font-family: 'Londrina Solid', cursive;
+  grid-column: 1 / 2;
+  grid-row: 2 / 3;
+  margin-top: 0;
+  margin-bottom: 0;
+  align-self: center;
+  font-family: "Londrina Solid", cursive;
 `;
 
 const Date = styled.p`
-	grid-column: 4 / 5;
-	grid-row: 2 / 3;
-	margin-top: 0;
-	margin-bottom: 0;
-    align-self: center;
-    font-family: 'Londrina Solid', cursive;
+  grid-column: 4 / 5;
+  grid-row: 2 / 3;
+  margin-top: 0;
+  margin-bottom: 0;
+  align-self: center;
+  font-family: "Londrina Solid", cursive;
 `;
 
 const Subtitle = styled.p`
-    border-top: 3px solid black;
-    box-sizing: content-box;
-    padding-top: 3px;
-	width: 100%;
-	margin: 0 auto;
-	grid-column: 1 / 5;
-    grid-row: 3 / 4;
-    align-self: center;
-    font-family: 'Londrina Solid', cursive;
+  border-top: 3px solid black;
+  box-sizing: content-box;
+  padding-top: 3px;
+  width: 100%;
+  margin: 0 auto;
+  grid-column: 1 / 5;
+  grid-row: 3 / 4;
+  align-self: center;
+  font-family: "Londrina Solid", cursive;
 `;
 const Image = styled.img`
-    border: 3px solid black;
+  border: 3px solid black;
 `;
 
 export { Wrapper, Header, TitleTop, TitleBottom, Subtitle, Issue, Date, Image };
