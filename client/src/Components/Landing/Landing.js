8bf639672fdedfbb2f362b1393d1c9676c238430
Henry <hrmstockdale@gmail.com>
2019-06-20 16:49:31 +0100

    What is an Action Planner? Modal for landing, additional root div in index.html, QuestionMark folder and files for svg icon

--- client/src/Components/Landing/Landing.js
+++ client/src/Components/Landing/Landing.js
@@ -1,50 +1,70 @@
-import React from 'react';
-import { Button, Submit } from '../Buttons/Button.js';
-import Clouds from './Clouds/Clouds';
-import Smoke from './Smoke/Smoke';
-import { Modal, OpenModal } from '../Modal/Modal';
-import Sign from './Signpost/Sign';
-import * as S from './Landing.style';
+import React from "react";
+import { Button, Submit } from "../Buttons/Button.js";
+import Clouds from "./Clouds/Clouds";
+import Smoke from "./Smoke/Smoke";
+import { ModalAction, Modal, OpenModal } from "../Modal/Modal";
+import Sign from "./Signpost/Sign";
+import * as S from "./Landing.style";
+import QuestionMark from "./QuestionMark/QuestionMark";
 
-const Landing = (props) => {
-	return (
-		<S.Main>
-			<Clouds />
-			<h1>LSx</h1>
-			<h2>Action Planner</h2>
-			<S.ButtonContainer>
-				<Submit link="#signpost"> Start Learning </Submit>
-				<Button link="/action-plan"> Make Action Plan </Button>
-			</S.ButtonContainer>
+const Landing = props => {
+  return (
+    <S.Main>
+      <Clouds />
+      <h1>LSx</h1>
+      <h2>Action Planner</h2>
+      <S.ButtonContainer>
+        <Submit link='#signpost'> Start Learning </Submit>
+        <Button link='/action-plan'> Make Action Plan </Button>
+        <OpenModal
+          toggle={show => <QuestionMark visibility={show} />}
+          content={hide => (
+            <ModalAction>
+              <S.Button onClick={hide}>X</S.Button>
+              <h3>
+                <br />
+                <b> How Can You Achieve Better Air Quality as a Community?</b>
+              </h3>
+              <p>
+                <S.Link href='http://www.lsx.org.uk/' /> Once you have
+                identified air pollution hotspots in your local area, you can
+                start thinking about how you can help reduce air pollution
+                emissions and exposure. There are several routes to take, which
+                can be combined to increase your chances of success.
+              </p>
+            </ModalAction>
+          )}
+        />
+      </S.ButtonContainer>
       <Smoke />
-			<S.Nav id="signpost">
-				<S.NavDescription>
-					<h2>Home</h2>
-					<h3>
-						{' '}
-						Follow the signs to learn more about different ways you can affect
-						change in your community before you start your action plan
-					</h3>
-				</S.NavDescription>
-				<OpenModal
-					toggle={(show) => <Sign visibility={show} />}
-					content={(hide) => (
-						<Modal>
-							<S.Button onClick={hide}>X</S.Button>
-							<h2>About LSx</h2>
-							<p>
-								<S.Link href="http://www.lsx.org.uk/">
-									London Sustainability Exchange
-								</S.Link>{' '}
-								is a “think and do” charity which creates collaborations that
-								address the complex barriers to a sustainable London.
-							</p>
-						</Modal>
-					)}
-				/>
-			</S.Nav>
-		</S.Main>
-	);
+      <S.Nav id='signpost'>
+        <S.NavDescription>
+          <h2>Home</h2>
+          <h3>
+            {" "}
+            Follow the signs to learn more about different ways you can affect
+            change in your community before you start your action plan
+          </h3>
+        </S.NavDescription>
+        <OpenModal
+          toggle={show => <Sign visibility={show} />}
+          content={hide => (
+            <Modal>
+              <S.Button onClick={hide}>X</S.Button>
+              <h2>About LSx</h2>
+              <p>
+                <S.Link href='http://www.lsx.org.uk/'>
+                  London Sustainability Exchange
+                </S.Link>{" "}
+                is a “think and do” charity which creates collaborations that
+                address the complex barriers to a sustainable London.
+              </p>
+            </Modal>
+          )}
+        />
+      </S.Nav>
+    </S.Main>
+  );
 };
 
 export default Landing;
---
+++ client/src/Components/Landing/QuestionMark/QuestionMark.style.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const Icon = styled.svg`
+  padding-top: 10px;
+  height: auto;
+  width: 25px;
+  position: relative;
+  color: grey;
+  cursor: pointer;
+`;
+
+const Span = styled.span`
+  margin: 5px;
+  text-decoration: underline;
+  color: grey;
+  cursor: pointer;
+`;
+
+export { Icon, Span };
