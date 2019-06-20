b9a393e11a6f868b0278666c6bd58ecaa7805bfd
Joko Sanyang <jo.san@live.co.uk>
2019-06-20 14:39:27 +0100

    Remove broken tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -5,37 +5,9 @@ import App from './App';
 import {createMemoryHistory} from 'history';
 import {render, fireEvent} from '@testing-library/react';
 
-const renderWithRouter = (
-  ui,
-  {route = '/', history = createMemoryHistory({initialEntries: [route]})} = {},
-) => {
-  return {
-    ...render(<Router history={history}>{ui}</Router>),
-    // adding `history` to the returned utilities to allow us
-    // to reference it in our tests (just try to avoid using
-    // this to test implementation details).
-    history,
-  }
-}
-
 
 test('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
-
-test('full app rendering/navigating', () => {
-  const { container, getByText } = renderWithRouter(<App />);
-  expect(container.innerHTML).toMatch('Action Planner');
-  const planningButton = getByText(/Make Action Plan/i);
-  fireEvent.click(planningButton);
-  expect(container.innerHTML).toMatch(/All action starts with a plan/i)
-});
-
-test('404 route', () => {
-  const {container} = renderWithRouter(<App />, {
-    route: '/something-that-does-not-match',
-  })
-  expect(container.innerHTML).toMatch('No match')
-})
